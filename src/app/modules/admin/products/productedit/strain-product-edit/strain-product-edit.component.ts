7d0462e8899493ce6a394e6a32187a935aad74fc
Mark Phillips <cafecartelsystems@gmail.com>
2022-11-10 23:42:17 -0800

    Save and Exit

--- src/app/modules/admin/products/productedit/strain-product-edit/strain-product-edit.component.ts
+++ src/app/modules/admin/products/productedit/strain-product-edit/strain-product-edit.component.ts
@@ -11,6 +11,7 @@ import { FbProductsService } from 'src/app/_form-builder/fb-products.service';
 import { IItemType, ItemTypeService } from 'src/app/_services/menu/item-type.service';
 import { PriceCategoriesService } from 'src/app/_services/menu/price-categories.service';
 import { switchMap } from 'rxjs/operators';
+import { T } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-strain-product-edit',
@@ -179,7 +180,7 @@ export class StrainProductEditComponent implements OnInit {
   }
 
   updateItemExit(event) {
-    this.updateItem(event).pipe(switchMap ( data => {
+    this.action$ = this.updateItem(event).pipe(switchMap ( data => {
       this.performingAction = false;
       this.onCancel(event);
       return of(data);
